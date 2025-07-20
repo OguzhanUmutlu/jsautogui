cbfe57ad5c38c7cb424b6b33865fe469682540be
Oğuzhan <oguzhan.umutlu123@gmail.com>
2025-07-21 01:23:18 +0300

    Fixed MacOS

--- src/system/osx.cpp
+++ src/system/osx.cpp
@@ -142,43 +142,185 @@ bool f_mouse_scroll(unsigned long x, unsigned long y) {
     return true;
 }
 
-bool f_press_key(bool is_ascii, int got) {
-    CGEventSourceRef eventSource = kCGEventSourceStateHIDSystemState;
-    CGEventRef keyDownEvent = CGEventCreateKeyboardEvent(eventSource, (CGKeyCode) 0, true);
-    CGEventKeyboardSetUnicodeString(keyDownEvent, 1, &got);
-    CGEventPost(kCGHIDEventTap, keyDownEvent);
-    CFRelease(keyDownEvent);
+static const CGKeyCode MODIFIERS[] = {
+    0, // placeholder
+    kVK_ANSI_Equal, // ↗
+    kVK_Option,
+    kVK_Option,
+    kVK_RightOption,
+    0, // placeholder
+    kVK_Delete,
+    0, // placeholder
+    kVK_F20,
+    kVK_F21,
+    kVK_F22,
+    kVK_F23,
+    kVK_F24,
+    kVK_F15, // placeholder
+    kVK_CapsLock,
+    kVK_Clear,
+    kVK_JIS_Yen, // best fit
+    kVK_Control,
+    kVK_Control,
+    kVK_RightControl,
+    kVK_ANSI_Period,
+    kVK_ForwardDelete,
+    kVK_ForwardDelete,
+    kVK_ANSI_Slash,
+    kVK_DownArrow,
+    kVK_End,
+    kVK_Return,
+    kVK_Escape,
+    kVK_Escape,
+    0, // placeholder
+    kVK_F1,
+    kVK_F10,
+    kVK_F11,
+    kVK_F12,
+    kVK_F13,
+    kVK_F14,
+    kVK_F15,
+    kVK_F16,
+    kVK_F17,
+    kVK_F18,
+    kVK_F19,
+    kVK_F2,
+    kVK_F20,
+    kVK_F21,
+    kVK_F22,
+    kVK_F23,
+    kVK_F24,
+    kVK_F3,
+    kVK_F4,
+    kVK_F5,
+    kVK_F6,
+    kVK_F7,
+    kVK_F8,
+    kVK_F9,
+    0, // placeholder
+    kVK_JIS_Kana, // best fit
+    kVK_JIS_Kana,
+    kVK_JIS_Eisu,
+    kVK_Help,
+    kVK_Home,
+    kVK_Help, // mac has no Insert—reuse Help
+    0, // placeholder
+    kVK_JIS_Kana,
+    kVK_JIS_Eisu,
+    kVK_F14,
+    kVK_F15,
+    kVK_F16,
+    kVK_F17,
+    kVK_LeftArrow,
+    kVK_ModeSwitch,
+    kVK_ANSI_Asterisk,
+    kVK_F18,
+    kVK_JIS_Yen,
+    kVK_ANSI_0,
+    kVK_ANSI_1,
+    kVK_ANSI_2,
+    kVK_ANSI_3,
+    kVK_ANSI_4,
+    kVK_ANSI_5,
+    kVK_ANSI_6,
+    kVK_ANSI_7,
+    kVK_ANSI_8,
+    kVK_ANSI_9,
+    kVK_NumLock,
+    kVK_PageDown,
+    kVK_PageUp,
+    kVK_Pause,
+    kVK_PageDown,
+    kVK_PageUp,
+    kVK_Play,
+    kVK_Previous,
+    kVK_PrintScreen,
+    kVK_PrintScreen,
+    kVK_PrintScreen,
+    kVK_PrintScreen,
+    kVK_PrintScreen,
+    kVK_Return,
+    kVK_RightArrow,
+    kVK_ScrollLock,
+    kVK_ANSI_5, // best fit
+    kVK_ANSI_Minus, // best fit
+    kVK_Shift,
+    kVK_Shift,
+    kVK_RightShift,
+    kVK_Power,
+    kVK_Space,
+    kVK_Stop,
+    kVK_ANSI_Minus,
+    kVK_Tab,
+    kVK_UpArrow,
+    kVK_VolumeDown,
+    kVK_VolumeMute,
+    kVK_VolumeUp,
+    kVK_Command,
+    kVK_Command,
+    kVK_RightCommand,
+    kVK_JIS_Yen,
+    kVK_Command,
+    kVK_Option,
+    kVK_Option,
+    kVK_RightOption
+};
 
-    CGEventRef keyUpEvent = CGEventCreateKeyboardEvent(eventSource, (CGKeyCode) 0, false);
-    CGEventKeyboardSetUnicodeString(keyUpEvent, 1, &got);
-    CGEventPost(kCGHIDEventTap, keyUpEvent);
-    CFRelease(keyUpEvent);
+static void PostKey(CGKeyCode keycode, bool keyDown) {
+    CGEventRef e = CGEventCreateKeyboardEvent(nullptr, keycode, keyDown);
+    CGEventPost(kCGHIDEventTap, e);
+    CFRelease(e);
+}
+
+bool f_keys_press(const KeyPressInfo *ch, size_t amount) {
+    for (size_t i = 0; i < amount; i++) {
+        auto [down, up, mode, key] = ch[i];
+        switch (mode) {
+            case KeyPressMode::ASCII: {
+                if (down) {
+                    CGEventRef e = CGEventCreateKeyboardEvent(nullptr, 0, true);
+                    UniChar c = static_cast<char>(key);
+                    CGEventKeyboardSetUnicodeString(e, 1, &c);
+                    CGEventPost(kCGHIDEventTap, e);
+                    CFRelease(e);
+                }
+                if (up) {
+                    CGEventRef e = CGEventCreateKeyboardEvent(nullptr, 0, false);
+                    UniChar c = static_cast<char>(key);
+                    CGEventKeyboardSetUnicodeString(e, 1, &c);
+                    CGEventPost(kCGHIDEventTap, e);
+                    CFRelease(e);
+                }
+                break;
+            }
+            case KeyPressMode::UNICODE: {
+                if (down) {
+                    CGEventRef e = CGEventCreateKeyboardEvent(nullptr, 0, true);
+                    UniChar u = key;
+                    CGEventKeyboardSetUnicodeString(e, 1, &u);
+                    CGEventPost(kCGHIDEventTap, e);
+                    CFRelease(e);
+                }
+                if (up) {
+                    CGEventRef e = CGEventCreateKeyboardEvent(nullptr, 0, false);
+                    UniChar u = key;
+                    CGEventKeyboardSetUnicodeString(e, 1, &u);
+                    CGEventPost(kCGHIDEventTap, e);
+                    CFRelease(e);
+                }
+                break;
+            }
+            case KeyPressMode::SPECIAL: {
+                if (key >= sizeof(MODIFIERS) / sizeof(*MODIFIERS))
+                    continue;
+                CGKeyCode vk = MODIFIERS[key];
+                if (down) PostKey(vk, true);
+                if (up) PostKey(vk, false);
+                break;
+            }
+        }
+    }
     return true;
-}
-
-bool f_key_down(bool is_ascii, int got) {
-    CGEventSourceRef eventSource = kCGEventSourceStateHIDSystemState;
-    CGEventRef keyDownEvent = CGEventCreateKeyboardEvent(eventSource, (CGKeyCode) 0, true);
-    CGEventKeyboardSetUnicodeString(keyDownEvent, 1, &got);
-    CGEventPost(kCGHIDEventTap, keyDownEvent);
-    CFRelease(keyDownEvent);
-}
-
-bool f_key_up(bool is_ascii, int got) {
-    CGEventSourceRef eventSource = kCGHIDEventSourceStateHIDSystemState;
-    CGEventRef keyUpEvent = CGEventCreateKeyboardEvent(eventSource, (CGKeyCode) 0, false);
-    CGEventKeyboardSetUnicodeString(keyUpEvent, 1, &got);
-    CGEventPost(kCGHIDEventTap, keyUpEvent);
-    CFRelease(keyUpEvent);
-    return true;
-}
-
-int f_show_message_box(char *textBuf, char *captionBuf, int flags) {
-    return 0;
-}
-
-int f_show_prompt() {
-    return 0;
 }
 
 #endif
